6f23b2856f231afc547f45c764466073659c219d
AyuAnjar <102272367+AyuAnjar@users.noreply.github.com>
2025-07-13 16:29:48 +0700

    ganti halaman upload

--- pages/1_Upload_Data.py
+++ pages/1_Upload_Data.py
@@ -46,12 +46,18 @@ st.markdown("""
 st.title("📂 Upload Data Penjualan")
 
 # Deskripsi Awal
+# st.markdown("""
+# <div class="info-box">
+#     <p class="upload-note">
+#     Silakan unggah file <strong>CSV</strong> berisi data penjualan minuman kopi kemasan siap minum. 
+#     Pastikan file memiliki kolom penting seperti: <code>id_detail_transaksi_penjualan</code>, <code>no_transaksi_penjualan</code>, <code>kode_barang</code>, <code>nama_barang</code> dan <code>qty</code>.
+#     </p>
+# </div>
+# """, unsafe_allow_html=True)
 st.markdown("""
-<div class="info-box">
-    <p class="upload-note">
-    Silakan unggah file <strong>CSV</strong> berisi data penjualan minuman kopi kemasan siap minum. 
-    Pastikan file memiliki kolom penting seperti: <code>id_detail_transaksi_penjualan</code>, <code>no_transaksi_penjualan</code>, <code>kode_barang</code>, <code>nama_barang</code> dan <code>qty</code>.
-    </p>
+<div class="info-box upload-note">
+📤 <strong>Silakan unggah file CSV</strong> berisi data penjualan minuman kopi kemasan siap minum.  
+Pastikan file memiliki kolom penting seperti: `id_detail_transaksi_penjualan`, `no_transaksi_penjualan`, `kode_barang`, `nama_barang`, dan `qty`.
 </div>
 """, unsafe_allow_html=True)
 
